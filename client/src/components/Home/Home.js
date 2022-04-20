0d9444b3247b739ea92c8442fab9f8bc993f7c58
mgenovski <mgenovski@gmail.com>
2022-04-20 22:43:11 +0300

    Add hero

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import './Home.css';
 
 const Home = () => {
     return (
         <>
             <div className='hero'>
-                <h1>Heading</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <Button as={Link} to="/home"> Text Goes Here </Button>
+                <div className='hero-text'>
+                    <h1>Heading</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <Button component={Link} variant="contained" to="/search">Search</Button>
+                </div>
             </div>
             <div className='favorites'>
                 <h2>Your Favorites</h2>
