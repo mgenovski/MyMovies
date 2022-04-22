b8b57ddf7aff3b8566f70d6bb5d11e48c7dcccfc
mgenovski <mgenovski@gmail.com>
2022-04-22 18:21:35 +0300

    Change notes endpoint

--- server/index.js
+++ server/index.js
@@ -33,5 +33,5 @@ async function start() {
 
     app.get('/', (req, res) => res.json({ message: 'REST service operational'}));
 
-    app.listen(3030, () => console.log('REST service started on port 3030'));
+    app.listen(3030, () => console.log('REST service started on port 3030 - http://localhost:3030'));
 }
